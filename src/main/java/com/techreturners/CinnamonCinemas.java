b69ed51f3e55172e1909bc00796c4a30a5782b71
Saranya Govind <saranya789@gmail.com>
2023-02-22 22:51:21 +0000

    First TestCaseAdded check successful Allocation to allocate 1 seat

--- src/main/java/com/techreturners/CinnamonCinemas.java
+++ src/main/java/com/techreturners/CinnamonCinemas.java
@@ -1,4 +1,14 @@
 package com.techreturners;
 
 public class CinnamonCinemas {
+    private int totalAvailabeSeats;
+
+    public CinnamonCinemas( int totalAvailableSeats){
+        this.totalAvailabeSeats = totalAvailableSeats;
+
+    }
+
+    public boolean allocateSeats(int seatRequested) {
+        return true;
+    }
 }
--- src/test/java/com/techreturners/CinnamonCinemasTest.java
+++ src/test/java/com/techreturners/CinnamonCinemasTest.java
@@ -1,10 +1,28 @@
+
 package com.techreturners;
 
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CinnamonCinemasTest {
+
+    private CinnamonCinemas cinema;
+
+    @BeforeEach
+    public void setup(){
+        cinema = new CinnamonCinemas(15);
+    }
+
+    @Test
+    public void checkSuccessfulAllocation(){
+        int seatRequested = 1;
+        assertTrue(cinema.allocateSeats(seatRequested));
+
+    }
+
+
 
 }
