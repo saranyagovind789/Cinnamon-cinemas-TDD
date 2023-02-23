67747acd2f64e478cd8ea6ab07128dbf93fe31fb
Saranya Govind <saranya789@gmail.com>
2023-02-23 22:14:35 +0000

    Unsuccesful allocation test added

--- src/test/java/com/techreturners/CinnamonCinemasTest.java
+++ src/test/java/com/techreturners/CinnamonCinemasTest.java
@@ -5,6 +5,7 @@ package com.techreturners;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CinnamonCinemasTest {
@@ -20,6 +21,13 @@ public class CinnamonCinemasTest {
     public void checkSuccessfulAllocation(){
         int seatRequested = 1;
         assertTrue(cinema.allocateSeats(seatRequested));
+
+    }
+
+    @Test
+    public void checkUnSuccessfulAllocation(){
+        int seatRequested = 16;
+        assertFalse(cinema.allocateSeats(seatRequested));
 
     }
 
