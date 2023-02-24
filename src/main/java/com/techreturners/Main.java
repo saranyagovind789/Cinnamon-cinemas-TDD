ececf0c265f98ddd0042da296cb9712119a9e7ca
Saranya Govind <saranya789@gmail.com>
2023-02-24 15:13:18 +0000

    main method added

--- src/main/java/com/techreturners/Main.java
+++ src/main/java/com/techreturners/Main.java
@@ -1,7 +1,22 @@
 package com.techreturners;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CinnamonCinemas cinema = new CinnamonCinemas(15);
+        Random rand = new Random();
+        boolean successfulAllocation;
+        System.out.println("****Welcome to Cinnamon Cinemas random seat allocation application****");
+
+        do{
+            int numberOfSeats = rand.nextInt(3) + 1;
+            System.out.println("The number of seats to be allocated " + numberOfSeats);
+            successfulAllocation = cinema.allocateSeats(numberOfSeats);
+            if(successfulAllocation) System.out.println(cinema.getListOfSeatsAllocated());
+            else System.out.println("Sorry seats cannot be allocated. Not enough available seats left 😭");
+            System.out.println();
+        }while (successfulAllocation);
     }
 }
