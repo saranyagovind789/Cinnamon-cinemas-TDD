325284a31bdf5530103a106dcdb8b6c3fda05d9d
Saranya Govind <saranya789@gmail.com>
2023-02-24 14:52:42 +0000

    Parameterized test added

--- src/main/java/com/techreturners/CinnamonCinemas.java
+++ src/main/java/com/techreturners/CinnamonCinemas.java
@@ -1,14 +1,37 @@
 package com.techreturners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CinnamonCinemas {
     private int totalAvailabeSeats;
+    private final List<String> allocatedSeatList;
+    private final boolean[][] seatMap;
+
 
     public CinnamonCinemas( int totalAvailableSeats){
         this.totalAvailabeSeats = totalAvailableSeats;
+        allocatedSeatList = new ArrayList<>();
+        seatMap = new boolean[3][5];
 
     }
 
     public boolean allocateSeats(int seatRequested) {
-        return true;
+        boolean ableToAllocate = false;
+        if(seatRequested > totalAvailabeSeats) return false;
+        for(int row = 0; row < seatMap.length; row++){
+            for(int seat = 0; seat< seatMap[0].length; seat++){
+                if(!seatMap[row][seat] && seatRequested > 0){
+                    seatMap[row][seat] = true;
+                    ableToAllocate = true;
+                    totalAvailabeSeats--;
+                    seatRequested--;
+
+                }
+            }
+            if(seatRequested == 0) break;
+        }
+        return ableToAllocate  && totalAvailabeSeats>0;
+       // return true;
     }
 }
--- src/test/java/com/techreturners/CinnamonCinemasTest.java
+++ src/test/java/com/techreturners/CinnamonCinemasTest.java
@@ -4,6 +4,9 @@ package com.techreturners;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,17 +17,20 @@ public class CinnamonCinemasTest {
 
     @BeforeEach
     public void setup(){
-        cinema = new CinnamonCinemas(15);
+       cinema = new CinnamonCinemas(15);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 14, 15})
     public void checkSuccessfulAllocation(){
+        cinema = new CinnamonCinemas(15);
         int seatRequested = 1;
         assertTrue(cinema.allocateSeats(seatRequested));
 
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {0,18})
     public void checkUnSuccessfulAllocation(){
         int seatRequested = 16;
         assertFalse(cinema.allocateSeats(seatRequested));
